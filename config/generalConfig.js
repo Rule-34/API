b016f6a956c6ac9e37d1d554605702a35a3a4bb5
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2019-11-28 18:23:35 +0100

    Heroku concurrency

--- config/generalConfig.js
+++ config/generalConfig.js
@@ -3,6 +3,7 @@ const generalConfig = {
   env: process.env.NODE_ENV || 'production',
   host: process.env.HOST || 'http://localhost/',
   port: process.env.PORT || 8000,
+  workers = process.env.WEB_CONCURRENCY || 1,
 }
 
 module.exports = generalConfig
