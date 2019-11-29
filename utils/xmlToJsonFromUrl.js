4dca6e241d45272ea4922320ddb84ae781f39db2
Tuan Anh Tran <me@tuananh.org>
2019-11-29 14:43:24 +0700

    feat: use camaro

--- utils/xmlToJsonFromUrl.js
+++ utils/xmlToJsonFromUrl.js
@@ -1,7 +1,18 @@
-const httpGet = require('../utils/HttpGet.js'),
-  xmlToJson = require('../utils/xmlToJson.js'),
-  jsonCleaner = require('../utils/jsonCleaner.js')
+const httpGet = require('../utils/HttpGet.js')
+const jsonCleaner = require('../utils/jsonCleaner.js')
+const { transform } = require('camaro')
 
+const template = {
+  count: 'number(/posts/@count)',
+  posts: ['/posts/post', {
+    id: 'number(@id)',
+    high_res_file: '@file_url',
+    low_res_file: '@sample_url',
+    preview_file: '@preview_url',
+    tags: '@tags',
+    type: ''
+  }]
+}
 async function xmlToJsonFromUrl(url) {
   // console.log(url)
   // First get XML from url
@@ -9,11 +20,11 @@ async function xmlToJsonFromUrl(url) {
   // console.log('XML DATA IS', xmlData)
 
   // Then transform to Json
-  const jsonData = await xmlToJson(xmlData)
+  const jsonData = await transform(xmlData, template)
   // console.log('JSON DATA IS', jsonData)
 
   // Then beautify json
-  const cleanJson = await jsonCleaner(jsonData)
+  const cleanJson = jsonCleaner(jsonData)
 
   // And return it
   return cleanJson
