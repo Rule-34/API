a5abb232f27ae088a36fbc15a985906a52d20dd2
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2019-11-29 09:46:11 +0100

    Formatted with prettier

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 // Heroku Concurrency
 const throng = require('throng'),
-// Requirements
+  // Requirements
   generalConfig = require('./config/generalConfig')
 
-throng({
-  workers: generalConfig.workers,
-  lifetime: Infinity
-}, start)
+throng(
+  {
+    workers: generalConfig.workers,
+    lifetime: Infinity,
+  },
+  start
+)
 
 function start() {
-const generalConfig = require('./config/generalConfig'),
-  configPackage = require('./package.json'),
-  debug = require('debug')(configPackage.name),
-  app = require('./config/express')
+  const generalConfig = require('./config/generalConfig'),
+    configPackage = require('./package.json'),
+    debug = require('debug')(configPackage.name),
+    app = require('./config/express')
 
-// Initialize server
-const server = app.listen(app.get('port'), function() {
-  debug(
-    `Express server listening on port ${server.address().port} in mode ${
-      generalConfig.env
-    } in host ${server.address().address}`
-  )
-})
-
-
+  // Initialize server
+  const server = app.listen(app.get('port'), function() {
+    debug(
+      `Express server listening on port ${server.address().port} in mode ${
+        generalConfig.env
+      } in host ${server.address().address}`
+    )
+  })
 }
