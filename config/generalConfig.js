14b8968366938526e4a20c3a52a808556b937cfc
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2019-11-29 14:52:44 +0100

    Explained debug env

--- config/generalConfig.js
+++ config/generalConfig.js
@@ -1,6 +1,7 @@
 // Defines defaults if they are available from ENV
 const generalConfig = {
   env: process.env.NODE_ENV || 'production',
+  debug: process.env.DEBUG || undefined, // This is just a dummy constant so you know you have to set a DEBUG env for debug messages to show
   host: process.env.HOST || 'http://localhost/',
   port: process.env.PORT || 8000,
   workers: process.env.WEB_CONCURRENCY || 1,
