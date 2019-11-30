53f86db7361db0413ed8aa935ae8f0be9501cd8e
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2019-11-30 18:17:57 +0100

    Better default info

--- routes/default.js
+++ routes/default.js
@@ -1,14 +1,24 @@
-var express = require('express')
+var express = require('express'),
+  generalConfig = require('../config/generalConfig')
 var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.json({
     message:
       'This is an API wrapper made for https://r34.app, it gathers content from the next sites',
-    rule34_xxx: 'https://rule34.xxx/',
-    rule34_paheal_net: 'https://rule34.paheal.net/',
-    lolibooru_moe: 'https://lolibooru.moe/',
+    rule34_xxx: {
+      domain: 'https://rule34.xxx/',
+      api: generalConfig.host + 'xxx/',
+    },
+    rule34_paheal_net: {
+      domain: 'https://rule34.paheal.net/',
+      api: generalConfig.host + 'paheal/',
+    },
+    lolibooru_moe: {
+      domain: 'https://lolibooru.moe/',
+      api: generalConfig.host + 'loli/',
+    },
   })
 })
 
