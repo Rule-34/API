ce4757a97db6fd5ab61737b9557969d72914672b
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2019-12-03 15:11:28 +0100

    Sanitizing posts queries

--- routes/xxx/posts.js
+++ routes/xxx/posts.js
@@ -1,21 +1,45 @@
 const express = require('express'),
   domainConfig = require('./domainConfig'),
   xmlToJsonFromUrl = require('../../utils/xmlToJsonFromUrl.js'),
-  router = express.Router()
+  router = express.Router(),
+  { check, validationResult } = require('express-validator')
 
 /* GET posts. */
-router.get('/', async (req, res) => {
-  // console.dir(result)
+router.get(
+  '/',
+  [
+    check('limit')
+      .isInt()
+      .optional(),
+    check('pid')
+      .isInt()
+      .optional(),
+    check('tags')
+      .isString()
+      .optional(),
+    check('score')
+      .isInt()
+      .optional(),
+  ],
+  async function(req, res) {
+    // Validation
 
-  // Get the requested parameters and create a url to request data with it
-  const requestUrl = applyUrlParameters(req)
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() })
+    }
 
-  // Process through wich the xml request gets transformed to optimized json
-  let jsonResult = await xmlToJsonFromUrl(requestUrl, 'posts', 'xxx')
+    // Get the requested parameters and create a url to request data with it
+    const requestUrl = applyUrlParameters(req)
 
-  // Reply to the client
-  res.json(jsonResult)
-})
+    // Process through wich the xml request gets transformed to optimized json
+    let jsonResult = await xmlToJsonFromUrl(requestUrl, 'posts', 'xxx')
+
+    // Reply to the client
+    res.json(jsonResult)
+  }
+)
 
 // Separated applying of query parameters
 function applyUrlParameters(req) {
