d099584853a0d91f8337251777ab13073247436d
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2020-02-06 10:45:29 +0100

    Created single post viewing for loli
    Although its not supported and just returns an error

--- routes/loli/domainConfig.js
+++ routes/loli/domainConfig.js
@@ -6,4 +6,6 @@ const domainConfig = {
   tagApiUrl: 'https://lolibooru.moe/tag/index.json',
 }
 
+// API help page --> https://lolibooru.moe/help/api
+
 module.exports = domainConfig
---
+++ routes/loli/singlePost.js
@@ -0,0 +1,16 @@
+const express = require('express'),
+  router = express.Router(),
+  { check, validationResult } = require('express-validator')
+
+/* GET posts. */
+router.get('/', [check('id').isInt()], async function(req, res) {
+  // Finds the validation errors in this request and wraps them in an object with handy functions
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() })
+  }
+
+  res.json({ Error: "This site doesn't allow to search for single posts" })
+})
+
+module.exports = router
