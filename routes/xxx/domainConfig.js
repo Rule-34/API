4ba3d25c5c3aedf567ff622776b5c1052e1c6147
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2020-02-06 10:31:57 +0100

    Created single post viewing for xxx

--- routes/xxx/domainConfig.js
+++ routes/xxx/domainConfig.js
@@ -6,4 +6,6 @@ const domainConfig = {
   tagApiUrl: 'https://rule34.xxx/autocomplete.php?',
 }
 
+// API help page --> https://rule34.xxx/index.php?page=help&topic=dapi
+
 module.exports = domainConfig
