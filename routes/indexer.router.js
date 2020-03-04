f2f9a8eba661655cae7d18f95ebdff0198275d4c
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2020-03-04 12:15:04 +0100

    Dynamically load routes

--- routes/indexer.router.js
+++ routes/indexer.router.js
@@ -1,30 +1,24 @@
 const express = require('express'),
   router = express.Router(),
-  // Import all routes // TODO: use glob to match *.route files
+  // Import all routes
   defaultRouter = require('./default'),
-  // proxyRouter = require('./proxy'), DEPRECATED because we're using a Cloudflare worker to do this
-  xxxRoutes = require('./xxx/index.routes.js'),
-  pahealRoutes = require('./paheal/index.routes.js'),
-  danbooruRoutes = require('./danbooru/index.routes.js'),
-  gelbooruRoutes = require('./gelbooru/index.routes.js'),
-  loliRoutes = require('./loli/index.routes.js'),
-  e621Routes = require('./e621/index.routes.js')
+  // debug = require('debug')(`Indexer`),
+  fs = require('fs')
 
 // All routes are added here
 router
   /* GET /health-check - Check service health */
   // .get('/health-check', (req, res) => res.send('OK'))
 
-  // Default screen
+  // Default router
   .use('/', defaultRouter)
 
-  // Booru routes
-  .use('/xxx/', xxxRoutes)
-  .use('/paheal/', pahealRoutes)
-  .use('/danbooru/', danbooruRoutes)
-  .use('/gelbooru/', gelbooruRoutes)
-  .use('/loli/', loliRoutes)
-  .use('/e621/', e621Routes)
+// Dynamic routes
+fs.readdirSync(__dirname, { withFileTypes: true })
+  .filter(dir => dir.isDirectory())
+  .map(dir =>
+    router.use(`/${dir.name}`, require(`./${dir.name}/index.routes.js`))
+  )
 
 // Export default
 module.exports = router
