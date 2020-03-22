ff4789b77b7463b569a4cd12b39d2f12664e7482
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2020-03-22 18:21:35 +0100

    Added dotEnv

--- server/config/generalConfig.js
+++ server/config/generalConfig.js
@@ -1,7 +1,13 @@
+require('dotenv').config()
+
 // Defines defaults if they are available from ENV
 const generalConfig = {
   env: process.env.NODE_ENV || 'production',
-  debug: process.env.DEBUG || undefined, // This is just a dummy constant so you know you have to set a DEBUG env for debug messages to show
+
+  // This is just a dummy constant so you know you have to set a DEBUG env for debug messages to show
+  debug: process.env.DEBUG || undefined,
+
+  // Server configuration
   host: process.env.HOST || 'http://localhost:8000/',
   port: process.env.PORT || 8000,
   workers: process.env.WEB_CONCURRENCY || 1,
