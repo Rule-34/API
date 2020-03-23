c0d64f2360f27de7d15ad0b5e83328cb35c1bf0c
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2020-03-23 16:58:52 +0100

    Changed API endpoints from httpS to http

--- server/routes/boorus/paheal/domainConfig.js
+++ server/routes/boorus/paheal/domainConfig.js
@@ -1,9 +1,9 @@
 // Defines defaults for domain
 const domainConfig = {
   routeUrl: 'paheal/',
-  baseUrl: 'https://rule34.paheal.net/',
-  apiUrl: 'https://rule34.paheal.net/api/danbooru/',
-  tagApiUrl: 'https://rule34.paheal.net/api/internal/autocomplete?',
+  baseUrl: 'http://rule34.paheal.net/',
+  apiUrl: 'http://rule34.paheal.net/api/danbooru/',
+  tagApiUrl: 'http://rule34.paheal.net/api/internal/autocomplete?',
 }
 
 // API help page --> It has no documentation at all
--- server/routes/boorus/xxx/domainConfig.js
+++ server/routes/boorus/xxx/domainConfig.js
@@ -1,9 +1,9 @@
 // Defines defaults for domain
 const domainConfig = {
   routeUrl: 'xxx/',
-  baseUrl: 'https://rule34.xxx/',
-  apiUrl: 'https://rule34.xxx/index.php?page=dapi&s=',
-  tagApiUrl: 'https://rule34.xxx/autocomplete.php?',
+  baseUrl: 'http://rule34.xxx/',
+  apiUrl: 'http://rule34.xxx/index.php?page=dapi&s=',
+  tagApiUrl: 'http://rule34.xxx/autocomplete.php?',
 }
 
 // API help page --> https://rule34.xxx/index.php?page=help&topic=dapi
