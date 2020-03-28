10d50f76ce3c4ffec3ff9cfbb3a712619d41e669
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-03-28 20:11:07 +0100

    Working basic version

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,20 @@
-import errorHandler from "errorhandler";
-
-import app from "./app";
+import app from './app'
 
 /**
  * Error Handler. Provides full stack - remove for production
  */
-app.use(errorHandler());
 
 /**
  * Start Express server.
  */
-const server = app.listen(app.get("port"), () => {
-    console.log(
-        "  App is running at http://localhost:%d in %s mode",
-        app.get("port"),
-        app.get("env")
-    );
-    console.log("  Press CTRL-C to stop\n");
-});
+const server = app.listen(app.get('port'), () => {
+  console.log(
+    `
+Express server
+Running on http://localhost:${app.get('port')}
+In ${app.get('env')} mode
+`
+  )
+})
 
-export default server;
+export default server
