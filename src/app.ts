7bae8dff0502dd706174a184c7d14e84ea143900
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-03-28 20:30:42 +0100

    Base version with all middleware

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,94 @@
 import express from 'express'
-
-import errorHandler from 'errorhandler'
-import compression from 'compression'
+// Utils
 import bodyParser from 'body-parser'
+import compression from 'compression'
+import logger from 'morgan'
+// Security
+import cors from 'cors'
+import helmet from 'helmet'
+import apicache from 'apicache'
+import rateLimit from 'express-rate-limit'
+// Errors
+import errorHandler from 'errorhandler'
 
 // Create Express server
 const app = express()
 
+// Create cache middleware
+const cache = apicache.middleware
+
+// Create Rate limiter middleware
+const rateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 225, // 15 requests per minute
+})
+
 // Express configuration
+app.set('trust proxy', 1)
 app.set('port', process.env.PORT || 3000)
 
-app.use(compression())
-app.use(bodyParser.json())
+// Express middleware
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(compression({ threshold: 0 }))
+app.use(helmet())
 
-app.use(errorHandler())
+// Middleware for development
+switch (process.env.NODE_ENV) {
+  case 'development':
+    // CORS
+    app.use(
+      cors({
+        origin: '*',
+        methods: ['GET'],
+      })
+    )
+
+    // Logging
+    app.use(logger('dev'))
+    break
+
+  case 'production':
+  default:
+    // CORS
+    app.use(
+      cors({
+        origin: 'https://r34.app',
+        methods: ['GET'],
+      })
+    )
+
+    // Logging
+    app.use(
+      logger('dev', {
+        skip: function (req, res) {
+          return res.statusCode < 400
+        },
+      })
+    )
+
+    // Cache
+    app.use(cache('5 minutes'))
+
+    app.get('/cache/performance', (req, res) => {
+      res.json(apicache.getPerformance())
+    })
+
+    app.get('/cache/index', (req, res) => {
+      res.json(apicache.getIndex())
+    })
+
+    // Rate limit
+    app.use(rateLimiter)
+    break
+}
 
 /**
- * OAuth authentication routes. (Sign in)
+ * Routes
  */
+// app.use(baseRouter)
+
+//  Error handler
+app.use(errorHandler())
 
 export default app
