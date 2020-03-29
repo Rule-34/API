cfb010f1b1e1f338dcb93354a0e4bf5b2bd7681c
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-03-29 10:54:11 +0200

    Corrected dynamic route import

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,13 +2,15 @@
 import { Router } from 'express'
 import fs from 'fs'
 
+// Create router
 const router = Router()
 
+// Add static routes
 router.use('/', require('./default'))
 
-// Dynamic routes
-// fs.readdirSync(__dirname + '/booru', { withFileTypes: true })
-//   .filter((dir) => dir.isDirectory())
-//   .map((dir) => router.use(`/${dir.name}`, require(`./${dir.name}`)))
+// Add dynamic routes
+fs.readdirSync(__dirname + '/booru', { withFileTypes: true })
+  .filter((dir) => dir.isDirectory())
+  .map((dir) => router.use(`/${dir.name}`, require(`./booru/${dir.name}`)))
 
 export default router
