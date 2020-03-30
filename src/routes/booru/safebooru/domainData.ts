3dd1d92b6eee36c7f30e6041d4e255e37e5011b2
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-03-30 19:39:24 +0200

    Added Safebooru

---
+++ src/routes/booru/safebooru/domainData.ts
@@ -0,0 +1,10 @@
+export default {
+  url: 'https://safebooru.org',
+  short: 'safebooru/',
+
+  postsApi: 'https://safebooru.org/index.php?page=dapi&s=post&q=index',
+  singlePostApi: 'https://safebooru.org/index.php?page=dapi&s=post&q=index&id=',
+  tagsApi: 'https://safebooru.org/autocomplete.php?',
+}
+
+// API help page --> https://safebooru.org/index.php?page=help&topic=dapi
