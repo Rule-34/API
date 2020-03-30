082cb1afff1b803043477673953990158da744ef
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-03-30 11:36:18 +0200

    Added query validation

---
+++ src/middleware/query-validation/index.ts
@@ -0,0 +1,57 @@
+import { Request, Response, NextFunction } from 'express'
+import { query, validationResult, ValidationChain } from 'express-validator'
+
+// Init
+import Debug from 'debug'
+const debug = Debug(`Server:middleware query validation`)
+
+export const postsValidation = (): ValidationChain[] => {
+  return [
+    query('limit').isInt().optional(),
+    query('pid').isInt().optional(),
+    query('tags').isString().optional(),
+    query('rating').isString().optional(),
+    query('score').isInt().optional(),
+    query('corsProxy').isBoolean().toBoolean().optional(),
+  ]
+}
+
+export const singlePostValidation = (): ValidationChain[] => {
+  return [
+    query('id').isInt().notEmpty(),
+    query('corsProxy').isBoolean().toBoolean().optional(),
+  ]
+}
+
+export const tagsValidation = (): ValidationChain[] => {
+  return [
+    query('tag').isString().notEmpty(),
+    query('limit').isInt().optional(),
+    query('order').isString().optional(),
+    query('pid').isInt().optional(),
+  ]
+}
+
+export const queryValidate = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  const errors = validationResult(req)
+
+  debug('\n\nlol\n\n')
+
+  // Check if there is any error
+  if (!errors.isEmpty()) {
+    // Create array of errors
+    const extractedErrors: Array<object> = []
+    errors.array().map((err) => extractedErrors.push({ [err.param]: err.msg }))
+
+    next(new Error(JSON.stringify(extractedErrors)))
+    return
+  }
+
+  // Continue if theres no errors
+  next()
+  return
+}
--- src/routes/booru/danbooru/index.ts
+++ src/routes/booru/danbooru/index.ts
@@ -1,7 +1,21 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { Router } from 'express'
+import {
+  postsValidation,
+  singlePostValidation,
+  tagsValidation,
+  queryValidate,
+} from '@/middleware/query-validation'
 
 const router = Router()
+
+/**
+ * Middleware
+ */
+router
+  .use('*/posts', postsValidation(), queryValidate)
+  .use('*/single-post', singlePostValidation(), queryValidate)
+  .use('*/tags', tagsValidation(), queryValidate)
 
 /*
  ** All routes are used here
