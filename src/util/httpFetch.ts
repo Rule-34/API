662a857bc2dc5bfd55699abdbc6b3b71f2ba0799
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-03-30 20:08:40 +0200

    Decode fetched data

--- src/util/httpFetch.ts
+++ src/util/httpFetch.ts
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch'
+import he from 'he'
 
 // Init
 import Debug from 'debug'
@@ -6,19 +7,24 @@ const debug = Debug('Server:util Fetch')
 
 // Gets the content from the passed url and returns it
 export default async (url: string): Promise<string> => {
+  // Encode url
+  // url = he.encode(url)
+
+  debug(url)
+
   const data = await fetch(url, {
     headers: {
       'User-Agent':
         'Rule 34 API (https://github.com/VoidlessSeven7/Rule-34-API)',
     },
   })
-    .then((res) => {
+    .then(async (res) => {
       // Check for HTTP status errors
       if (!res.ok) {
         throw new Error('Network response was not ok')
       }
 
-      return res.text()
+      return he.decode(await res.text())
     })
     .catch((error) => {
       throw new Error(`Fetch: ${error}`)
