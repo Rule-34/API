4e51c0228f34e90a0ae80a921a1758acd18494a5
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-03-30 19:06:39 +0200

    Added gelbooru

---
+++ src/routes/booru/gelbooru/domainData.ts
@@ -0,0 +1,10 @@
+export default {
+  url: 'https://gelbooru.com',
+  short: 'gelbooru/',
+
+  postsApi: 'https://gelbooru.com/index.php?page=dapi&s=post&q=index',
+  singlePostApi: 'https://gelbooru.com/index.php?page=dapi&s=post&q=index',
+  tagsApi: 'https://gelbooru.com/index.php?page=dapi&s=tag&q=index',
+}
+
+// API help page --> https://gelbooru.com/index.php?page=wiki&s=view&id=18780
---
+++ src/routes/booru/gelbooru/tags.ts
@@ -0,0 +1,54 @@
+import { Request, Response } from 'express'
+
+// Configuration
+import domainData from './domainData'
+
+// Util
+import fetchAndTransform from '@/util/fetchAndTransform'
+
+// Init
+import Debug from 'debug'
+const debug = Debug(`Server:route gelbooru tags`)
+
+/**
+ * Helper function for building an URL
+ * @param req Request for extracting queries
+ */
+function applyUrlParameters(req: Request): string {
+  // Default query parameters
+  const tag: string = req.query.tag,
+    limit: number = req.query.limit || 25,
+    // pageId: number = req.query.pid || 1,
+    order: string = req.query.order || 'count'
+
+  return (
+    domainData.tagsApi +
+    '&json=1' +
+    '&name_pattern=' +
+    tag +
+    '&limit=' +
+    limit +
+    '&orderby=' +
+    order
+  )
+}
+
+/**
+ * Route
+ */
+module.exports = async (req: Request, res: Response): Promise<void> => {
+  // Get the requested parameters and create a url to request data with it
+  const requestUrl: string = applyUrlParameters(req)
+  debug(requestUrl)
+
+  // Process through wich the xml request gets transformed to optimized json
+  const jsonResult: object = await fetchAndTransform({
+    url: requestUrl,
+    template: 'tags',
+    domain: 'gelbooru',
+    isJson: true,
+  })
+
+  // Reply
+  res.json(jsonResult)
+}
