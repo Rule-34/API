1577cd3cc00558d1dbb355cfa1f7f64cc92f465c
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-03-30 19:28:07 +0200

    Added e621

---
+++ src/routes/booru/e621/domainData.ts
@@ -0,0 +1,10 @@
+export default {
+  url: 'https://e621.net',
+  short: 'e621/',
+
+  postsApi: 'https://e621.net/posts.json',
+  singlePostApi: 'https://e621.net/posts/',
+  tagsApi: 'https://e621.net/tags.json',
+}
+
+// API help page --> https://e621.net/help/api
