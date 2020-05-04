4d330f3019ca537eaaf9dc704dd52534b4dd502e
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-05-04 15:23:24 +0200

    Updated cors proxy url to use the new one

--- src/configuration/index.ts
+++ src/configuration/index.ts
@@ -1,4 +1,4 @@
 export default {
   host: process.env.HOST || 'http://localhost:8100',
-  corsProxyUrl: 'https://cors-proxy.rule34app.workers.dev/?q=',
+  corsProxyUrl: 'https://cors-proxy.r34.app/?q=',
 }
