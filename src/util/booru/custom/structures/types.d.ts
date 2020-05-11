3872d328c3a6b5e411ee88605d2be8dee2c1e464
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-05-11 15:44:02 +0200

    Added interfaces for Post request and response

---
+++ src/util/booru/custom/structures/types.d.ts
@@ -0,0 +1,85 @@
+export interface PostResponse {
+  id: number
+  score: number
+  high_res_file: {
+    url: string
+    height: number
+    width: number
+  }
+  low_res_file: {
+    url: string
+    height: number
+    width: number
+  }
+  preview_file: {
+    url: string
+    height: number
+    width: number
+  }
+  tags: string[]
+  source: string[]
+  rating: string
+  type: string
+}
+
+export interface PostRequest {
+  /**
+   * ID
+   */
+  id: number
+
+  /**
+   * Score
+   */
+  score: number | { total: number }
+
+  /**
+   * Media
+   */
+
+  // High res file
+  high_res_file?: { url: string; height: number; width: number } // Transformed XML
+  file_url?: string
+  file?: { url: string; height: number; width: number }
+
+  height?: number
+  image_height?: number
+  width?: number
+  image_width?: number
+
+  // Low res file
+  low_res_file?: { url: string; height: number; width: number } // Transformed XML
+  large_file_url?: string
+  sample?: { url: string; height: number; width: number }
+  sample_url?: string
+
+  sample_height?: number
+  sample_width?: number
+
+  // Preview res file
+  preview_file?: { url: string; height: number; width: number } // Transformed XML
+  preview_file_url?: string
+  preview?: { url: string; height: number; width: number }
+  preview_url?: string
+
+  preview_height?: number
+  preview_width?: number
+
+  /**
+   * Tags
+   */
+  tags?: string | [string[]]
+  tag_string?: string
+
+  /**
+   * Source
+   */
+  sources?: Array<string>
+  source?: string
+  source_url?: string
+
+  /**
+   * Rating
+   */
+  rating: string
+}
