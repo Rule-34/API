eec12eba13b358f1eb038bc67edfafdc856c2706
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-05-11 17:48:40 +0200

    Refactored class into function as we arent going to inherit anything

--- src/util/booru/custom/structures/Post.ts
+++ src/util/booru/custom/structures/Post.ts
@@ -1,198 +1,200 @@
+// Definitions
 import { PostResponse, PostRequest } from './types'
 
-export class Post {
-  // Fixes related
-  public booru: string
+// Classes
+import { CustomError } from '@/util/classes'
 
-  // Post related
-  public id: number
-  public score: number
-  public high_res_file: PostResponse['high_res_file']
-  public low_res_file: PostResponse['low_res_file']
-  public preview_file: PostResponse['preview_file']
-  public tags: string[]
-  public source: string[]
-  public rating: string
-  public type: string
-  constructor(booru: string, fetchedPostData: PostRequest) {
-    /*
-     * Booru Type
-     */
-    this.booru = booru
+// Init
+import Debug from 'debug'
+const debug = Debug(`Server:util Post Cleaner`)
 
-    /*
-     * ID
-     */
-    this.id = fetchedPostData.id
-
-    /*
-     * Score
-     */
-    this.score = Number(fetchedPostData.score ?? fetchedPostData.score.total)
-
-    /*
-     * File URLs
-     */
-    this.high_res_file = {
-      url:
-        // rule34.xxx | rule34.paheal.net | gelbooru | safebooru - XML transformed boorus
-        fetchedPostData.high_res_file.url ??
-        // lolibooru | danbooru
-        fetchedPostData.file_url ??
-        // E621 - Modern boorus
-        fetchedPostData.file.url,
-
-      height:
-        // rule34.xxx | rule34.paheal.net | gelbooru | safebooru - XML transformed boorus
-        fetchedPostData.high_res_file.height ??
-        // lolibooru
-        fetchedPostData.height ??
-        // E621 - Modern boorus
-        fetchedPostData.file.height ??
-        // danbooru.donmai.us
-        fetchedPostData.image_height,
-
-      width:
-        // rule34.xxx | rule34.paheal.net | gelbooru | safebooru - XML transformed boorus
-        fetchedPostData.high_res_file.width ??
-        // lolibooru
-        fetchedPostData.width ??
-        // E621 - Modern boorus
-        fetchedPostData.file.width ??
-        // danbooru.donmai.us
-        fetchedPostData.image_width,
-    }
-
-    this.low_res_file = {
-      url:
-        // rule34.xxx | rule34.paheal.net | gelbooru | safebooru - XML transformed boorus
-        fetchedPostData.low_res_file.url ??
-        // lolibooru
-        fetchedPostData.sample_url ??
-        // E621 - Modern boorus
-        fetchedPostData.sample.url ??
-        // danbooru.donmai.us
-        fetchedPostData.large_file_url,
-
-      height:
-        // rule34.xxx | rule34.paheal.net | gelbooru | safebooru - XML transformed boorus
-        fetchedPostData.low_res_file.height ??
-        // lolibooru
-        fetchedPostData.sample_height ??
-        // E621 - Modern boorus
-        fetchedPostData.sample.height,
-
-      width:
-        // rule34.xxx | rule34.paheal.net | gelbooru | safebooru - XML transformed boorus
-        fetchedPostData.low_res_file.width ??
-        // lolibooru
-        fetchedPostData.sample_width ??
-        // E621 - Modern boorus
-        fetchedPostData.sample.width,
-    }
-
-    this.preview_file = {
-      url:
-        // rule34.xxx | rule34.paheal.net | gelbooru | safebooru - XML transformed boorus
-        fetchedPostData.preview_file.url ??
-        // E621
-        fetchedPostData.preview.url ??
-        // danbooru.donmai.us
-        fetchedPostData.preview_file_url,
-
-      height:
-        // rule34.xxx | rule34.paheal.net | gelbooru | safebooru - XML transformed boorus
-        fetchedPostData.preview_file.height ??
-        // lolibooru
-        fetchedPostData.preview_height ??
-        // E621
-        fetchedPostData.preview.height,
-
-      width:
-        // rule34.xxx | rule34.paheal.net | gelbooru | safebooru - XML transformed boorus
-        fetchedPostData.preview_file.width ??
-        // lolibooru
-        fetchedPostData.preview_width ??
-        // E621
-        fetchedPostData.preview.width,
-    }
-
-    /*
-     * Tags
-     */
-
-    this.tags =
-      // rule34.xxx | rule34.paheal.net | gelbooru | safebooru - XML transformed boorus
-      (fetchedPostData.tags as string)?.trim().split(' ') ??
-      // danbooru.donmai.us
-      fetchedPostData.tag_string?.trim().split(' ')
-
-    if (!this.tags.length) {
-      const tempTags: string[] = []
-
-      // E621 - TODO: will probably break something
-      ;(fetchedPostData.tags as [string[]]).forEach((tagContainer) => {
-        tempTags.concat(tagContainer)
-      })
-    }
-
-    /*
-     * Source
-     */
-    this.source =
-      // Usual
-      [fetchedPostData.source] ?? [fetchedPostData.source_url] ?? // E621
-      //  Unknown
-      fetchedPostData.sources
-
-    /*
-     * Rating
-     */
-    switch (fetchedPostData.rating) {
-      case 'e':
-        this.rating = 'explicit'
-        break
-
-      case 'q':
-      case 'suggestive': // Derpibooru
-        this.rating = 'questionable'
-        break
-
-      case 's':
-        this.rating = 'safe'
-        break
-
-      case 'u':
-        this.rating = 'unrated'
-        break
-
-      default:
-        this.rating = 'unknown'
-        break
-    }
-
-    /*
-     * Media type
-     */
-    this.type = /\.(webm|mp4|ogg)$/.test(this.high_res_file.url)
-      ? 'video'
-      : 'image'
+export function createPostFromData(
+  booru: string,
+  fetchedPostData: PostRequest
+): PostResponse {
+  const tmpJSON: PostResponse = {
+    id: undefined,
+    score: undefined,
+    high_res_file: {
+      url: undefined,
+      height: undefined,
+      width: undefined,
+    },
+    low_res_file: {
+      url: undefined,
+      height: undefined,
+      width: undefined,
+    },
+    preview_file: {
+      url: undefined,
+      height: undefined,
+      width: undefined,
+    },
+    tags: undefined,
+    source: undefined,
+    rating: undefined,
+    type: undefined,
   }
 
-  /**
-   * Converts Class to object
+  /*
+   * ID
    */
-  public toObject(): PostResponse {
-    return {
-      id: this.id,
-      score: this.score,
-      high_res_file: this.high_res_file,
-      low_res_file: this.low_res_file,
-      preview_file: this.preview_file,
-      tags: this.tags,
-      source: this.source,
-      rating: this.rating,
-      type: this.type,
-    }
+  tmpJSON.id = fetchedPostData.id
+
+  /*
+   * Score
+   */
+  tmpJSON.score = Number(fetchedPostData.score ?? fetchedPostData.score.total)
+
+  /*
+   * File URLs
+   */
+  tmpJSON.high_res_file = {
+    url:
+      // rule34.xxx | rule34.paheal.net | gelbooru | safebooru - XML transformed boorus
+      fetchedPostData.high_res_file.url ??
+      // lolibooru | danbooru
+      fetchedPostData.file_url ??
+      // E621 - Modern boorus
+      fetchedPostData.file.url,
+
+    height:
+      // rule34.xxx | rule34.paheal.net | gelbooru | safebooru - XML transformed boorus
+      fetchedPostData.high_res_file.height ??
+      // lolibooru
+      fetchedPostData.height ??
+      // E621 - Modern boorus
+      fetchedPostData.file.height ??
+      // danbooru.donmai.us
+      fetchedPostData.image_height,
+
+    width:
+      // rule34.xxx | rule34.paheal.net | gelbooru | safebooru - XML transformed boorus
+      fetchedPostData.high_res_file.width ??
+      // lolibooru
+      fetchedPostData.width ??
+      // E621 - Modern boorus
+      fetchedPostData.file.width ??
+      // danbooru.donmai.us
+      fetchedPostData.image_width,
   }
+
+  tmpJSON.low_res_file = {
+    url:
+      // rule34.xxx | rule34.paheal.net | gelbooru | safebooru - XML transformed boorus
+      fetchedPostData.low_res_file.url ??
+      // lolibooru
+      fetchedPostData.sample_url ??
+      // E621 - Modern boorus
+      fetchedPostData.sample.url ??
+      // danbooru.donmai.us
+      fetchedPostData.large_file_url,
+
+    height:
+      // rule34.xxx | rule34.paheal.net | gelbooru | safebooru - XML transformed boorus
+      fetchedPostData.low_res_file.height ??
+      // lolibooru
+      fetchedPostData.sample_height ??
+      // E621 - Modern boorus
+      fetchedPostData.sample.height,
+
+    width:
+      // rule34.xxx | rule34.paheal.net | gelbooru | safebooru - XML transformed boorus
+      fetchedPostData.low_res_file.width ??
+      // lolibooru
+      fetchedPostData.sample_width ??
+      // E621 - Modern boorus
+      fetchedPostData.sample.width,
+  }
+
+  tmpJSON.preview_file = {
+    url:
+      // rule34.xxx | rule34.paheal.net | gelbooru | safebooru - XML transformed boorus
+      fetchedPostData.preview_file.url ??
+      // E621
+      fetchedPostData.preview.url ??
+      // danbooru.donmai.us
+      fetchedPostData.preview_file_url,
+
+    height:
+      // rule34.xxx | rule34.paheal.net | gelbooru | safebooru - XML transformed boorus
+      fetchedPostData.preview_file.height ??
+      // lolibooru
+      fetchedPostData.preview_height ??
+      // E621
+      fetchedPostData.preview.height,
+
+    width:
+      // rule34.xxx | rule34.paheal.net | gelbooru | safebooru - XML transformed boorus
+      fetchedPostData.preview_file.width ??
+      // lolibooru
+      fetchedPostData.preview_width ??
+      // E621
+      fetchedPostData.preview.width,
+  }
+
+  /*
+   * Tags
+   */
+
+  tmpJSON.tags =
+    // rule34.xxx | rule34.paheal.net | gelbooru | safebooru - XML transformed boorus
+    (fetchedPostData.tags as string)?.trim().split(' ') ??
+    // danbooru.donmai.us
+    fetchedPostData.tag_string?.trim().split(' ')
+
+  if (!tmpJSON.tags.length) {
+    const tempTags: string[] = []
+
+    // E621 - TODO: will probably break something
+    ;(fetchedPostData.tags as [string[]]).forEach((tagContainer) => {
+      tempTags.concat(tagContainer)
+    })
+  }
+
+  /*
+   * Source
+   */
+  tmpJSON.source =
+    // Usual
+    [fetchedPostData.source] ?? [fetchedPostData.source_url] ?? // E621
+    //  Unknown
+    fetchedPostData.sources
+
+  /*
+   * Rating
+   */
+  switch (fetchedPostData.rating) {
+    case 'e':
+      tmpJSON.rating = 'explicit'
+      break
+
+    case 'q':
+    case 'suggestive': // Derpibooru
+      tmpJSON.rating = 'questionable'
+      break
+
+    case 's':
+      tmpJSON.rating = 'safe'
+      break
+
+    case 'u':
+      tmpJSON.rating = 'unrated'
+      break
+
+    default:
+      tmpJSON.rating = 'unknown'
+      break
+  }
+
+  /*
+   * Media type
+   */
+  tmpJSON.type = /\.(webm|mp4|ogg)$/.test(tmpJSON.high_res_file.url)
+    ? 'video'
+    : 'image'
+
+  return tmpJSON
+}
+
 }
