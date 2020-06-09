d47bd1222cfdf40fbdc6fa1e917b20fa35930758
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-06-09 14:16:17 +0200

    build: revert back jest import
    As it needs the .js version of tsconfig

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { pathsToModuleNameMapper } = require('ts-jest/utils')
-const { compilerOptions } = require('./tsconfig.json')
-
 module.exports = {
   testEnvironment: 'node',
   preset: 'ts-jest',
 
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  moduleNameMapper: {
+    '^@src/(.*)$': '<rootDir>/src/$1',
+  },
 }
