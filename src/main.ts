7f56750ca55f43c3b717ff4b27fa7ad857265e5a
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-12-04 16:59:24 +0100

    feat: configure ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // Transform to DTO type
+      whitelist: true, // Remove unnecessary properties
+      forbidNonWhitelisted: true,
+    })
+  )
 
   await app.listen(3000)
 }
