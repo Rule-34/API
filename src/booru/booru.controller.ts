64390380ad845106c0a3b48109f87720cd2edbc2
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-12-07 18:59:23 +0100

    feat: add helper function

--- src/booru/booru.controller.ts
+++ src/booru/booru.controller.ts
@@ -3,6 +3,25 @@ import {
   BooruTypesStringEnum,
 } from '@alejandroakbal/universal-booru-wrapper'
 import { BooruEndpointParamsDTO } from './dto/request-booru.dto'
+function getAPIClassByType(booruType: BooruTypesStringEnum) {
+  switch (booruType) {
+    case BooruTypesStringEnum.GELBOORU:
+      return Gelbooru
+
+    case BooruTypesStringEnum.PAHEAL:
+      return Paheal
+
+    // Moebooru and MyImouto are danbooru
+    case BooruTypesStringEnum.DANBOORU:
+      return Danbooru
+
+    case BooruTypesStringEnum.DANBOORU2:
+      return Danbooru2
+
+    case BooruTypesStringEnum.E621:
+      return E621
+  }
+}
 
 @Controller('booru')
 export class BooruController {
