bb5f5e79989e57ea6ea1071f9ad6a1e1d49fa603
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-12-07 18:21:22 +0100

    feat: switch to fastify

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  )
 
   app.useGlobalPipes(
     new ValidationPipe({
