233c9ae745e26026f700dbad21e7981bb0b3ed2f
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-12-17 12:23:08 +0100

    feat: simplify

--- src/booru/dto/request-booru.dto.ts
+++ src/booru/dto/request-booru.dto.ts
@@ -1,10 +1,12 @@
 import { BooruTypesStringEnum } from '@alejandroakbal/universal-booru-wrapper'
 import { IsDefined, IsIn, IsNotEmpty, IsString } from 'class-validator'
 
+const BooruTypesToArray = Object.values(BooruTypesStringEnum)
+
 export class BooruEndpointParamsDTO {
   @IsDefined()
   @IsNotEmpty()
   @IsString()
-  @IsIn(Object.values(BooruTypesStringEnum))
+  @IsIn(BooruTypesToArray)
   readonly booruType: BooruTypesStringEnum
 }
