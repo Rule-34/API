79ee3b2907f546cc754e3470324867f852b82286
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-12-18 12:06:12 +0100

    feat: move logic from controller to service

--- src/booru/booru.controller.ts
+++ src/booru/booru.controller.ts
@@ -1,45 +1,24 @@
-import {
-  BooruTypesStringEnum,
-  Danbooru,
-  Danbooru2,
-  E621,
-  Gelbooru,
-  Paheal,
-} from '@alejandroakbal/universal-booru-wrapper'
 import { Controller, Get, Param, Query } from '@nestjs/common'
 import { BooruEndpointParamsDTO } from './dto/request-booru.dto'
 import { booruOptionsDTO } from './dto/booru-queries.dto'
-
-function getAPIClassByType(booruType: BooruTypesStringEnum) {
-  switch (booruType) {
-    case BooruTypesStringEnum.GELBOORU:
-      return Gelbooru
-
-    case BooruTypesStringEnum.PAHEAL:
-      return Paheal
-
-    // Moebooru and MyImouto are danbooru
-    case BooruTypesStringEnum.DANBOORU:
-      return Danbooru
-
-    case BooruTypesStringEnum.DANBOORU2:
-      return Danbooru2
-
-    case BooruTypesStringEnum.E621:
-      return E621
-  }
-}
+import { BooruService } from './booru.service'
 
 @Controller('booru')
 export class BooruController {
+  private booruService: BooruService
+
+  constructor(booruService: BooruService) {
+    this.booruService = booruService
+  }
+
   @Get(':booruType/posts')
   GetPosts(
     @Param() params: BooruEndpointParamsDTO,
     @Query() queries: booruOptionsDTO
   ) {
-    const ApiClass = getAPIClassByType(params.booruType)
+    const booruClass = this.booruService.getApiClassByType(params.booruType)
 
-    const Api = new ApiClass({ base: queries.baseEndpoint })
+    const Api = new booruClass({ base: queries.baseEndpoint })
 
     // return Api.getPosts()
     return { params, queries }
--- src/booru/booru.service.ts
+++ src/booru/booru.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import {
+  BooruTypesStringEnum,
+  Danbooru,
+  Danbooru2,
+  E621,
+  Gelbooru,
+  Paheal,
+} from '@alejandroakbal/universal-booru-wrapper'
 
 @Injectable()
-export class BooruService {}
+export class BooruService {
+  public getApiClassByType(booruType: BooruTypesStringEnum) {
+    switch (booruType) {
+      case BooruTypesStringEnum.GELBOORU:
+        return Gelbooru
+
+      case BooruTypesStringEnum.PAHEAL:
+        return Paheal
+
+      // Moebooru and MyImouto are danbooru
+      case BooruTypesStringEnum.DANBOORU:
+        return Danbooru
+
+      case BooruTypesStringEnum.DANBOORU2:
+        return Danbooru2
+
+      case BooruTypesStringEnum.E621:
+        return E621
+    }
+  }
+}
