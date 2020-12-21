4b6dc91a8976fc2f6bba7f0987bfcac627fe509b
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-12-21 16:55:57 +0100

    feat: start gumroad auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { BooruModule } from './booru/booru.module';
+import { GumroadAuthenticationModule } from './gumroad-authentication/gumroad-authentication.module';
 
 @Module({
-  imports: [BooruModule],
+  imports: [BooruModule, GumroadAuthenticationModule],
 })
 export class AppModule {}
