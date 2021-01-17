0ee2b4e47091672eec82aca397f9ad02cd9ea698
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2021-01-17 13:08:17 +0100

    feat: add root controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
-import { BooruModule } from './booru/booru.module';
-import { GumroadAuthenticationModule } from './gumroad-authentication/gumroad-authentication.module';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { BooruModule } from './booru/booru.module'
+import { GumroadAuthenticationModule } from './gumroad-authentication/gumroad-authentication.module'
 
 @Module({
   imports: [BooruModule, GumroadAuthenticationModule],
+  controllers: [AppController],
 })
 export class AppModule {}
