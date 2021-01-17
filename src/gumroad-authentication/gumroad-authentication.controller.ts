d8a03d8b8d74681640b908d6ad6ecfb1f335b1a3
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2021-01-17 11:38:28 +0100

    feat: create gumroad functionality

--- src/gumroad-authentication/gumroad-authentication.controller.ts
+++ src/gumroad-authentication/gumroad-authentication.controller.ts
@@ -3,4 +3,34 @@ import { GumroadBodyDTO } from './dto/gumroad-body.dto'
 
 @Controller('auth/gumroad')
 export class GumroadAuthenticationController {
+  @Post('/')
+  async VerifyLicense(
+    @Body()
+    body: GumroadBodyDTO
+  ) {
+    const { product_permalink, license_key, increment_uses_count } = body
+
+    const gumroadLicenseVerificationEndpoint =
+      'https://api.gumroad.com/v2/licenses/verify'
+
+    const requestInit: RequestInit = {
+      method: 'POST',
+
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+
+      body:
+        'product_permalink=' +
+        product_permalink +
+        '&license_key=' +
+        license_key +
+        '&increment_uses_count=' +
+        increment_uses_count,
+    }
+
+    const request = new Request(gumroadLicenseVerificationEndpoint, requestInit)
+
+    return await fetch(request)
+  }
 }
