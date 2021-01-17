0083ce53f821256014b4f7cf88efaf99c72778c9
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2021-01-17 17:07:07 +0100

    feat: add config package so we can use environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common'
 import { BooruModule } from './booru/booru.module'
 import { AuthModule } from './auth/auth.module'
 import { AppController } from './app.controller'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [BooruModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, cache: true }),
+    BooruModule,
+    AuthModule,
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
