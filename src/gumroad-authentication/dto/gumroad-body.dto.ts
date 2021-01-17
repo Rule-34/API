e1d539d7f26e1c4fed4c03cc6b1679e31a73fc10
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2021-01-17 12:48:03 +0100

    feat: improve gumroad dto

--- src/gumroad-authentication/dto/gumroad-body.dto.ts
+++ src/gumroad-authentication/dto/gumroad-body.dto.ts
@@ -3,25 +3,38 @@ import {
   IsNotEmpty,
   IsOptional,
   IsString,
-  Max,
-  Min,
+  MaxLength,
+  MinLength,
 } from 'class-validator'
+import { Transform } from 'class-transformer'
 
 export class GumroadBodyDTO {
   @IsString()
   @IsNotEmpty()
-  @Min(1)
-  @Max(10)
+  @MinLength(1)
+  @MaxLength(20)
   readonly product_permalink: string
 
   @IsString()
   @IsNotEmpty()
-  @Min(1)
-  @Max(30)
+  @MinLength(1)
+  @MaxLength(50)
   readonly license_key: string
 
   @IsBoolean()
   @IsNotEmpty()
   @IsOptional()
-  readonly increment_uses_count: boolean = true
+  @Transform((value) => {
+    switch (value) {
+      case 'true':
+        return true
+
+      case 'false':
+        return false
+
+      default:
+        return true
+    }
+  })
+  readonly increment_uses_count: boolean
 }
