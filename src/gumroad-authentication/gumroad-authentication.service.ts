993a147aebf6090c13d9a57fd5ec5faad1ebf17f
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2021-01-17 12:43:52 +0100

    feat: create gumroad service

---
+++ src/gumroad-authentication/gumroad-authentication.service.ts
@@ -0,0 +1,8 @@
+import { HttpService, Injectable } from '@nestjs/common'
+import { AxiosRequestConfig, AxiosResponse } from 'axios'
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
+
+@Injectable()
+export class GumroadAuthenticationService {
+}
