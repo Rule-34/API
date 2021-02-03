72c77662c5bf783c66c6aa712a816d0b884f496a
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2021-02-03 19:42:30 +0100

    test: creeate test for authentication controller

---
+++ src/authentication/authentication.controller.spec.ts
@@ -0,0 +1,51 @@
+import { UnauthorizedException } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
+import { Test, TestingModule } from '@nestjs/testing'
+import { UsersModule } from '../users/users.module'
+import { AuthenticationController } from './authentication.controller'
+import { AuthenticationService } from './authentication.service'
+
+describe('AuthenticationController', () => {
+  let controller: AuthenticationController
+  let configService: ConfigService
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot(),
+        UsersModule,
+        JwtModule.registerAsync({
+          imports: [ConfigModule.forRoot()],
+          inject: [ConfigService],
+          useFactory: async (configService: ConfigService) => ({
+            secret: configService.get<string>('JWT_SECRET'),
+            signOptions: {
+              expiresIn: configService.get<string>('JWT_EXPIRATION_TIME'),
+            },
+          }),
+        }),
+      ],
+      controllers: [AuthenticationController],
+      providers: [AuthenticationService],
+    }).compile()
+
+    controller = module.get<AuthenticationController>(AuthenticationController)
+
+    configService = module.get<ConfigService>(ConfigService)
+  })
+
+  it('should be defined', () => {
+    expect.assertions(1)
+
+    expect(controller).toBeDefined()
+  })
+
+  describe('auth/profile', () => {
+    it('should fail with no Bearer token', () => {
+      expect.assertions(1)
+
+      expect(() => controller.profile({})).toThrowError(UnauthorizedException)
+    })
+  })
+})
