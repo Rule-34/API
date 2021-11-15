a37f0b636a70d2c73289d19bf536666c20b84e7c
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2021-11-15 19:34:51 +0100

    chore: fix eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,11 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
   root: true,
